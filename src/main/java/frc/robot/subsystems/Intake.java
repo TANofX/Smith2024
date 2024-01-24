a65a6e7e71581374c779712266e4efebbf714023
Gloria <mcalping@ccsdbraves.org>
2024-01-23 19:58:12 -0500

    Changes were made

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -17,7 +18,8 @@ import frc.robot.Constants;
 public class Intake extends AdvancedSubsystem {
   private final CANSparkMax intakeMotor = new CANSparkMax(Constants.Intake.intakeCANID, MotorType.kBrushless);
   private final CANSparkMax pivotIntakeMotor = new CANSparkMax(Constants.Intake.pivotIntakeCANID, MotorType.kBrushless);
-  private final SparkPIDController pivotController= pivotIntakeMotor.getPIDController();
+  private final SparkPIDController pivotController = pivotIntakeMotor.getPIDController();
+  private final SparkPIDController pivotIntakeController = pivotIntakeMotor.getPIDController();
   /** Creates a new Intake. */
   public Intake() {
     registerHardware("Intake Motor", intakeMotor);
@@ -25,11 +27,10 @@ public class Intake extends AdvancedSubsystem {
 
   }
   public void intakeGamePiece() {
-    intakeMotor.set(0.25);
-
+  pivotIntakeController.setReference(25, ControlType.kVelocity);
   }
   public void passGamePiece() {
-    pivotController.setReference());
+    pivotIntakeController.setReference(100, ControlType.kVelocity);
   }
 
   @Override
