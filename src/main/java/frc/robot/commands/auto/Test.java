9956618eb6442e6a089e3058470381a6ff833d3b
shirley <chens@ccsdbraves.org>
2024-01-27 13:07:00 -0500

    supposed working auto test code

--- src/main/java/frc/robot/commands/auto/Test.java
+++ src/main/java/frc/robot/commands/auto/Test.java
@@ -1,9 +1,16 @@
 package frc.robot.commands.auto;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.commands.SwerveDriveWithGamepad;
+import java.util.List;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.PathPlannerTrajectory;
 /*
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerTrajectory;
@@ -11,21 +18,22 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import java.util.List;
 import frc.robot.commands.auto.Autos;
 */
 
-
 public class Test extends SequentialCommandGroup {
   public Test(AutoBuilder autoBuilder) {
-    PathPlannerPath pathTest =
+    PathPlannerPath pathGroup = 
         PathPlannerPath.fromPathFile("New Path");
-        addCommands(
-            Commands.sequence(
-//              autoBuilder.resetPose(pathTest.getPoint(0)),
-//              autoBuilder.followPath(pathTest.getPoint(0)));
-                autoBuilder.,
-                autoBuilder.followPath(pathTest)));
+    addCommands(AutoBuilder.followPath(pathGroup));
+
+    // PathPlannerTrajectory trajectory = pathGroup.
+    
+    // List<PathPlannerTrajectory> pathPlannerTrajectory =
+    //     PathPlannerTrajectory(pathGroup, RobotContainer.swerve.getCurrentSpeeds(), RobotContainer.swerve.getPose().getRotation());
+    //         Commands.sequence(
+    //             autoBuilder.resetPose(),
+    //             autoBuilder.followPath(pathGroup.getPoint(0)));
   }
 }
 
