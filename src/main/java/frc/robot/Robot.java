08e8532edf3f25a968e4638d0f1f44a14ec7f6cd
David Russell <david@sayhii.io>
2024-03-13 10:14:13 -0400

    Added calibrate elevator to autocommand

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.CalibrateElevator;
 import frc.robot.commands.SwerveDriveWithGamepad;
 import frc.robot.commands.auto.Autos;
 
@@ -43,6 +44,9 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
     autoCommand = Autos.getAutonomousCommand();
     RobotContainer.swerve.removeDefaultCommand();
+
+    autoCommand = new CalibrateElevator().andThen(new CalibrateElevator()).andThen(autoCommand);
+    
     if (autoCommand != null) {
       autoCommand.schedule();
     }
